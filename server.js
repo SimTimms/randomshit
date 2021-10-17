d263f6c563ff27330fce32ff3899da8334cb4e50
Tim.Simms <tim-simms@hotmail.com>
2021-10-17 18:47:11 +0100

    Save

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ app.get('/photo', function (req, res) {
     <meta name="twitter:title" content="Top 10 Things Ever">
     <meta name="twitter:description" content="Up than 200 characters.">
     <meta name="twitter:creator" content="@creator_username">
+    <meta property="og:title" content="A Twitter for My Sister" />
+<meta property="og:description" content="In the early days, Twitter grew so quickly that it was almost impossible to add new features because engineers spent their time trying to keep the rocket ship from stalling." />
+<meta property="og:image" content="http://graphics8.nytimes.com/images/2011/12/08/technology/bits-newtwitter/bits-newtwitter-tmagArticle.jpg" />
     <meta name="twitter:image" content="http://placekitten.com/250/250">
     <meta name="twitter:domain" content="YourDomain.com">
     </head>
