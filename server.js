426b012b2059198f2e7b10b21a2a4a0c8dccd585
Tim.Simms <tim-simms@hotmail.com>
2021-10-17 18:35:29 +0100

    Save

--- server.js
+++ server.js
@@ -6,7 +6,16 @@ const port = process.env.PORT || 3000; // Heroku will need the PORT environment 
 
 app.get('/photo', function (req, res) {
   res.send(
-    `<img src="https://random-shit-store.s3.amazonaws.com/614b73c98a97c40c65957b89/10152161254.jpg">`
+    `<html><head><meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="@site_username">
+    <meta name="twitter:title" content="Top 10 Things Ever">
+    <meta name="twitter:description" content="Up than 200 characters.">
+    <meta name="twitter:creator" content="@creator_username">
+    <meta name="twitter:image" content="http://placekitten.com/250/250">
+    <meta name="twitter:domain" content="YourDomain.com">
+    </head>
+    <body>
+    <img src="https://random-shit-store.s3.amazonaws.com/614b73c98a97c40c65957b89/10152161254.jpg"></body></html>`
   );
 });
 app.use(express.static(path.join(__dirname, 'build')));
