44ad93e76a36713c29134caf1b4c1d1d6fde2ab8
Tim.Simms <tim-simms@hotmail.com>
2021-10-17 18:30:22 +0100

    Save

--- src/components/loaderIcon/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Icon } from '@material-ui/core';
-
-function LoadIcon() {
-  return <Icon style={{ fontSize: 18, padding: 10 }}>sync</Icon>;
-}
-
-export default LoadIcon;
