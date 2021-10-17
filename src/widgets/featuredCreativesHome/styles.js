2a05a6775de3222ef4ad691c92c906abf86bc402
Tim.Simms <tim-simms@hotmail.com>
2021-10-17 18:09:16 +0100

    Save

--- src/widgets/featuredCreativesHome/styles.js
+++
@@ -1,8 +0,0 @@
-import { makeStyles } from '@material-ui/core/styles';
-export const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'grid',
-    gridTemplateColumns: '1fr 1fr 1fr',
-    width: '100%',
-  },
-}));
