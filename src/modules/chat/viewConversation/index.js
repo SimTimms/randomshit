8bc0d93477d0939d007123fbbe43b78126957158
Tim.Simms <tim-simms@hotmail.com>
2021-12-21 22:19:30 +0000

    Build

--- src/modules/chat/viewConversation/index.js
+++
@@ -1,4 +0,0 @@
-import ConversationById from './conversationById';
-import ConversationByJob from './conversationByJob';
-
-export { ConversationById, ConversationByJob };
--- src/widgets/myPosts/index.js
+++
@@ -1,5 +0,0 @@
-import MyPosts from './myPosts';
-import MyPostForm from './myPostForm';
-import PublicPostsWidget from './publicPostsWidget';
-
-export { MyPosts, MyPostForm, PublicPostsWidget };
