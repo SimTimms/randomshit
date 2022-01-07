aa1513328738216556871507e44f949fe0561e06
Tim.Simms <tim-simms@hotmail.com>
2022-01-07 22:29:08 +0000

    Build

--- src/components/profileCard/components/index.js
+++
@@ -1,8 +0,0 @@
-import BgImg from './bgImg';
-import ProfileImg from './profileImg';
-import ProfileName from './profileName';
-import Badges from './badges';
-import InviteButton from './inviteButton';
-import InviteMenu from './inviteMenu';
-
-export { BgImg, ProfileImg, ProfileName, Badges, InviteButton, InviteMenu };
