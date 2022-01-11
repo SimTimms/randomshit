1f82de67c094da8c7e43055392da2ce0a48dd9e2
Tim.Simms <tim-simms@hotmail.com>
2022-01-11 09:47:32 +0000

    Build

--- src/assets/badge/index.js
+++
@@ -1,8 +0,0 @@
-import supporter from './supporter.png';
-import golden from './golden.png';
-import featured from './featured.png';
-import extraordinary from './extraordinary.png';
-import earlyBird from './earlyBird.png';
-import contractOne from './contractOne.png';
-
-export { supporter, golden, featured, extraordinary, earlyBird, contractOne };
--- src/modules/quotes/editQuote/components/index.js
+++
@@ -1,7 +0,0 @@
-import StartDate from './startDate';
-import Cost from './cost';
-import Notes from './notes';
-import QuoteSummary from './quoteSummary';
-import EndDate from './endDate';
-
-export { StartDate, EndDate, Cost, Notes, QuoteSummary };
