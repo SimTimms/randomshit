718ade4760117441e83848398cc00d659702efde
Tim.Simms <tim-simms@hotmail.com>
2022-01-13 21:53:52 +0000

    Save

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3eaec2ac0b64b1ae33c84e356e413322.js"
+  "/precache-manifest.1057c8b423152d501699fd977cdef641.js"
 );
 
 self.addEventListener('message', (event) => {
