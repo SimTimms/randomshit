0a2ee6607a3b88565418d5a3e7613d27a65a3601
Tim.Simms <tim-simms@hotmail.com>
2022-01-18 20:44:54 +0000

    Build

--- src/widgets/profileCard/components/index.js
+++
@@ -1,6 +0,0 @@
-import BgImg from './bgImg';
-import ProfileImg from './profileImg';
-import Badges from './badges';
-import Filters from './filters';
-
-export { BgImg, ProfileImg, Badges, Filters };
