4563bc42922f7d8aef47c33f4c53172cf66f0f97
Tim.Simms <tim-simms@hotmail.com>
2022-02-18 14:55:27 +0000

    Build

--- src/components/profileCard/index.js
+++
@@ -1,4 +0,0 @@
-import ProfileCard from './profileCard';
-import ProfileCardBlank from './profileCardBlank';
-
-export { ProfileCard, ProfileCardBlank };
--- src/layouts/app/views/appDashboard/getPosts.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export function getPosts(setPosts) {
-  return null;
-}
-
-export function getFeatured(setFeaturedArticle) {
-  return null;
-}
