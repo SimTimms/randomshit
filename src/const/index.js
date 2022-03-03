83c5bf2d487af7047dba0caebb2081c7bd272d73
Tim.Simms <tim-simms@hotmail.com>
2022-03-03 11:36:25 +0000

    Build

--- src/const/index.js
+++
@@ -1,8 +0,0 @@
-export const PAGES = {
-  home: 'home',
-  pickModelsPrimary: 'pick_models',
-  pickModelsSeconday: 'pick_models',
-  myModels: 'my_models',
-  myModelGallery: 'my_model_gallery',
-  createModel: 'create_model',
-};
--- src/layouts/app/views/appProfileEdit/initialState.js
+++
@@ -1,9 +0,0 @@
-export const initialState = {
-  _id: '',
-  name: '',
-  summary: '',
-  profileBG: '',
-  profileImg: '',
-  creativeTrue: false,
-  creatorTrue: false,
-};
