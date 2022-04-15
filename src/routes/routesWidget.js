2e847863251b01761b57dc0ff3f57eb4949e4ff8
Tim.Simms <tim-simms@hotmail.com>
2022-04-15 20:20:42 +0100

    Save

---
+++ src/routes/routesWidget.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import PreviewLayout from '../layouts/preview';
+import { PublicLayout } from '../layouts/public';
+import MyCharEmbed from '../MyCharEmbed';
+import ModelApp from '../layouts/app/modelApp';
+
+export default function WidgetRoutes({ theme, props: { ...props } }) {
+  return (
+    <Switch>
+      <Route
+        path="/"
+        render={(props) => <PublicLayout {...props} theme={theme} />}
+      />
+    </Switch>
+  );
+}
